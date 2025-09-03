2d2f9f4995ce8ea39f451174618f5271ba1f2c94
Mike James <mike.james@solatron.io>
2025-09-03 15:52:40 -0500

    Add associations for emails and visits in WixUser model

--- app/models/wix_user.rb
+++ app/models/wix_user.rb
@@ -2,6 +2,8 @@
 class WixUser < ApplicationRecord
   has_one :subscription
   has_many :features, through: :subscription
+  has_many :emails  # Add this line
+  has_many :visits  # Add this line
   
   validates :wix_id, presence: true, uniqueness: true
   validates :email, presence: true
